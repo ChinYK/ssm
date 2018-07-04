a77c4897aa22becc40db4a672011539641e5ea31
a1924x2018 <1924851085@qq.com>
2018-07-04 10:54:05 +0800

    userArea

---
+++ src/com/hbjc/domain/UserArea.java
@@ -0,0 +1,66 @@
+package com.hbjc.domain;
+
+import java.io.Serializable;
+
+/**
+ * com.hbjc.domain
+ * @author wangx
+ * 2018年7月4日上午9:33:02
+ * @version v1.0
+ */
+public class UserArea implements Serializable {
+
+
+
+	private String area;
+	private String count;
+	private double bili;
+	/**
+	 * @return the area
+	 */
+	public String getArea() {
+		return area;
+	}
+	/**
+	 * @param area the area to set
+	 */
+	public void setArea(String area) {
+		this.area = area;
+	}
+	/**
+	 * @return the count
+	 */
+	/**
+	 * @return the count
+	 */
+	/**
+	 * @return the bili
+	 */
+	public double getBili() {
+		return bili;
+	}
+	/**
+	 * @param bili the bili to set
+	 */
+	public void setBili(double bili) {
+		this.bili = bili;
+	}
+	/**
+	 * @return the count
+	 */
+	public String getCount() {
+		return count;
+	}
+	/**
+	 * @param count the count to set
+	 */
+	public void setCount(String count) {
+		this.count = count;
+	}
+
+
+
+
+
+
+}
